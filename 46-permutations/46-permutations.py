469374e744952ed79e49ac522374596cbf3241d9
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-03-01 16:34:11 +0530

    Time: 55 ms (56.02%), Space: 14.1 MB (79.81%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def find_all_permutations(nums,ds,ans,freq):
-            if len(ds) == len(nums):
-                ans.append(list(ds))
-            
-            for i in range(len(nums)):
-                if not(freq[i]):
-                    freq[i] = True
-                    ds.append(nums[i])
-                    find_all_permutations(nums,ds,ans,freq)
-                    ds.pop()
-                    freq[i] = False
+        def find_all_permutations(index,nums,ans):
+            if index == len(nums):
+                ans.append(list(nums))
+                
+            for i in range(index,len(nums)):
+                nums[index], nums[i] = nums[i], nums[index]
+                find_all_permutations(index+1,nums,ans);
+                nums[index], nums[i] = nums[i], nums[index]
                     
-        ds = []
+        # ds = []
         ans = []
-        freq = [False] * len(nums)
-        find_all_permutations(nums,ds,ans,freq)
+        # freq = [False] * len(nums)
+        find_all_permutations(0,nums,ans)
         return ans
             
