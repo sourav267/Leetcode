6df73ba77bca0e9a9926e35ed912cca385b38fd8
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-03-07 19:00:10 +0530

    Time: 1461 ms (13.72%), Space: 14.6 MB (80.95%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -2,16 +2,14 @@ class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n = len(matrix)
         for i in range(n):
-            setCheck = set()
+            setCheck1 = set()
+            setCheck2 = set()
             for j in range(n):
-                if matrix[i][j] in setCheck:return False
-                else:setCheck.add(matrix[i][j])
-        
-        for i in range(n):
-            setCheck = set()
-            for j in range(n):
-                if matrix[j][i] in setCheck:return False
-                else:setCheck.add(matrix[j][i])
+                if matrix[i][j] in setCheck1 or matrix[j][i] in setCheck2:return False
+                else:
+                    setCheck1.add(matrix[i][j])
+                    setCheck2.add(matrix[j][i])
+                
                     
         return True
                 
