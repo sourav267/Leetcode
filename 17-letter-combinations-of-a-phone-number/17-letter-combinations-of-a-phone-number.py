3b0f88d4ad8bf4fda6296166b97df0d5f690ec91
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-03-15 16:21:26 +0530

    Time: 28 ms (95.40%), Space: 13.8 MB (86.57%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,45 +1,22 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        map_arr = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
         
-        ans = ['']
-        for d in digits:
-            queue = [x+y for x in ans for y in map_arr[int(d)]]
-            ans = queue     
-        return ans if digits else []
-        
-        
-        # ans = []
-        # ans.append("")
-        # for i in range(0,len(digits)):
-        #     d = int(digits[i])
-        #     while len(ans[0]) == i:
-        #         t = ans.pop(0)
-        #         for c in map_arr[d]:
-        #             ans.append(t+c)
-        # return ans
-                
-        
-#         def solve(map_arr,n,digits,ans,comb,index):
-#             if len(comb) == n:
-#                 ans.append(comb)
-#                 return
-#             for i in range(index,n):
-#                 digit = digits[i]
-#                 for c in map_arr[int(digit)] :
-#                     solve(map_arr,n,digits,ans,comb+c,index+1)
-#                 return ans
-        
-        
-#         map_arr = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
-#         n = len(digits)
-#         comb = ""
-#         ans = []
-#         if n == 0: return []
-#         if n == 1:
-#             return list(map_arr[int(digits)])
-#         return(solve(map_arr,n,digits,ans,comb,0))
-                    
-                    
+        def solve(digits,n,ans,result,map_arr,index):
+            if len(ans) == n and index:
+                result.append(ans)
+                return
             
-        
+            for i in range(index,n):
+                value = map_arr[int(digits[i])]
+                for c in value:
+                    solve(digits,n,ans+c,result,map_arr,index+1)
+                return ans
+                    
+        result = []
+        map_arr = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+        if len(digits) == 0: return []
+        if len(digits) == 1:
+            return list(map_arr[int(digits)])
+        solve(digits,len(digits),"",result,map_arr,0)
+        return result
+            
