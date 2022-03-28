b3305c9ddc0d51bd9e43f6e7ad9ee1e508a6fa71
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-03-28 23:56:27 +0530

    Time: 202 ms (52.03%), Space: 53.4 MB (54.43%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,32 +6,37 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        
-        
-        def buildHelper(preorder,preS,preE,inorder,inS,inE,inMap):
-
-            if preS > preE or inS > inE :
-                return None
-
-            root = TreeNode(preorder[preS])
-            inRoot = inMap[root.val]
-            numsLeft = inRoot - inS
-
-            root.left = buildHelper(preorder,preS+1,preS+numsLeft,
-                                    inorder,inS,inRoot-1,
-                                    inMap)
-            root.right = buildHelper(preorder,preS + numsLeft + 1,preE,
-                                     inorder,inRoot + 1,inE,
-                                     inMap)
-
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0:ind])
+            root.right = self.buildTree(preorder, inorder[ind+1:])
             return root
         
-        inMap = dict()
-        for i,e in enumerate(inorder):
-            inMap[e] = i
+#         def buildHelper(preorder,preS,preE,inorder,inS,inE,inMap):
+
+#             if preS > preE or inS > inE :
+#                 return None
+
+#             root = TreeNode(preorder[preS])
+#             inRoot = inMap[root.val]
+#             numsLeft = inRoot - inS
+
+#             root.left = buildHelper(preorder,preS+1,preS+numsLeft,
+#                                     inorder,inS,inRoot-1,
+#                                     inMap)
+#             root.right = buildHelper(preorder,preS + numsLeft + 1,preE,
+#                                      inorder,inRoot + 1,inE,
+#                                      inMap)
+
+#             return root
+        
+#         inMap = dict()
+#         for i,e in enumerate(inorder):
+#             inMap[e] = i
             
-        root = buildHelper(preorder,0,len(preorder)-1,
-                           inorder,0,len(preorder)-1,
-                           inMap)
-        return root
+#         root = buildHelper(preorder,0,len(preorder)-1,
+#                            inorder,0,len(preorder)-1,
+#                            inMap)
+#         return root
         
