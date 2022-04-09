8ab2e81404325c5a1f264f07632ee6e5145f9286
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-09 19:42:10 +0530

    Time: 121 ms (70.86%), Space: 14.6 MB (97.40%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -3,23 +3,26 @@ class Solution:
 
         m = len(grid)
         n = len(grid[0])
-        dp = [[0 for i in range(n)] for j in range(m)]
+        # dp = [[0 for i in range(n)] for j in range(m)]
+        prev = [0 for i in range(n)] 
         
         for i in range(m):
+            temp = [0 for i in range(n)] 
             for j in range(n):
                 # print (dp)
-                if i == 0 and j == 0 : dp[i][j] = grid[i][j]
+                if i == 0 and j == 0 : temp[j] = grid[i][j]
                 
                 else:
                     up = grid[i][j]
-                    if i>0: up += dp[i-1][j]
+                    if i>0: up += prev[j]
                     else: up += 9999
                     
                     left = grid[i][j]
-                    if j>0: left += dp[i][j-1]
+                    if j>0: left += temp[j-1]
                     else: left += 9999
                     
-                    dp[i][j] = min(left,up)
+                    temp[j] = min(left,up)
+            prev =  temp
         
-        return dp[m-1][n-1]
+        return prev[-1]
         
