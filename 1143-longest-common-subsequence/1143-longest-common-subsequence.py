b49e3d00a678e5918fa8b8eb94f500fea5aa9ce5
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-15 14:47:37 +0530

    Time: 441 ms (80.93%), Space: 22.7 MB (58.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,24 +1,40 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        def findAnswer(s1,s2,i,j,dp):
+#         def findAnswer(s1,s2,i,j,dp):
 
-            if i == 0 or j == 0:
-                return 0
+#             if i == 0 or j == 0:
+#                 return 0
             
-            if dp[i][j] != -1:
-                return dp[i][j]
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
 
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = 1 + findAnswer(s1,s2,i-1,j-1,dp)
-                return dp[i][j]
+#             if s1[i-1] == s2[j-1]:
+#                 dp[i][j] = 1 + findAnswer(s1,s2,i-1,j-1,dp)
+#                 return dp[i][j]
 
-            dp[i][j] = max(findAnswer(s1,s2,i,j-1,dp),findAnswer(s1,s2,i-1,j,dp))
-            return dp[i][j]
-        
+#             dp[i][j] = max(findAnswer(s1,s2,i,j-1,dp),findAnswer(s1,s2,i-1,j,dp))
+#             return dp[i][j]
+
         n = len(text1)
         m = len(text2)
         dp = [[-1 for j in range(m+1)] for i in range(n+1)]
-        ans = findAnswer(text1,text2,n,m,dp)
-        return ans
+        s1 = text1
+        s2 = text2
+        for j in range(m+1):
+            dp[0][j] = 0
+
+        for i in range(n+1):
+            dp[i][0] = 0
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i-1][j])
+        # print(dp[n][m])
+        # ans = findAnswer(text1,text2,n,m,dp)
+        # return ans
+        return dp[n][m]
         
