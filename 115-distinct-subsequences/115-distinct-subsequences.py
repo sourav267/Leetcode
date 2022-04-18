a32976e2e6ef5925ee46fce626d505c6c791e699
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-18 06:52:55 +0530

    Time: 727 ms (56.93%), Space: 71.3 MB (34.13%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -21,7 +21,23 @@ class Solution:
             
         n=len(s)
         m=len(t)
-        dp=[[-1 for j in range(m+1)] for i in range(n+1)]
-        ans = f(n,m,s,t,dp)
-        return ans
+        dp=[[0 for j in range(m+1)] for i in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0]=1
+        
+        for j in range(1,m+1):
+            dp[0][j]=0
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1] == t[j-1]:
+                    pick_both = dp[i-1][j-1]
+                    pick_one = dp[i-1][j]
+                    dp[i][j] = pick_both + pick_one
+                else:
+                    dp[i][j] = dp[i-1][j] 
+        
+        # ans = f(n,m,s,t,dp)
+        return dp[n][m]
         
