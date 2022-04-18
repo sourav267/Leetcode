494884cebbe3ca9455ed10946a6413347c05f32d
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-18 07:05:27 +0530

    Time: 704 ms (59.93%), Space: 71.4 MB (29.29%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -1,24 +1,5 @@
 class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        
-        def f(i,j,s,t,dp):
-            if j==0:
-                return 1
-            if i==0:
-                return 0
-            
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            if s[i-1] == t[j-1]:
-                pick_both = f(i-1,j-1,s,t,dp)
-                pick_one = f(i-1,j,s,t,dp)
-                dp[i][j] = pick_both + pick_one
-                return dp[i][j]
-            else:
-                dp[i][j] = f(i-1,j,s,t,dp)
-                return dp[i][j] 
-            
+    def numDistinct(self, s: str, t: str) -> int:    
         n=len(s)
         m=len(t)
         dp=[[0 for j in range(m+1)] for i in range(n+1)]
@@ -26,8 +7,9 @@ class Solution:
         for i in range(n+1):
             dp[i][0]=1
         
-        for j in range(1,m+1):
-            dp[0][j]=0
+        # for base case when i = 0
+        # for j in range(1,m+1):
+        #     dp[0][j]=0
         
         for i in range(1,n+1):
             for j in range(1,m+1):
@@ -38,6 +20,23 @@ class Solution:
                 else:
                     dp[i][j] = dp[i-1][j] 
         
-        # ans = f(n,m,s,t,dp)
         return dp[n][m]
-        
+
+    
+#         def f(i,j,s,t,dp):
+#             if j==0:
+#                 return 1
+#             if i==0:
+#                 return 0
+            
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+            
+#             if s[i-1] == t[j-1]:
+#                 pick_both = f(i-1,j-1,s,t,dp)
+#                 pick_one = f(i-1,j,s,t,dp)
+#                 dp[i][j] = pick_both + pick_one
+#                 return dp[i][j]
+#             else:
+#                 dp[i][j] = f(i-1,j,s,t,dp)
+#                 return dp[i][j] 
