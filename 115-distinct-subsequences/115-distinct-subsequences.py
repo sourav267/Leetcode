ae6719506920750931c1f902be66c86663c78ce5
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-18 06:41:32 +0530

    Time: 585 ms (72.67%), Space: 24.1 MB (74.20%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -2,15 +2,15 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
         def f(i,j,s,t,dp):
-            if j<0:
+            if j==0:
                 return 1
-            if i<0:
+            if i==0:
                 return 0
             
             if dp[i][j] != -1:
                 return dp[i][j]
             
-            if s[i] == t[j]:
+            if s[i-1] == t[j-1]:
                 pick_both = f(i-1,j-1,s,t,dp)
                 pick_one = f(i-1,j,s,t,dp)
                 dp[i][j] = pick_both + pick_one
@@ -21,7 +21,7 @@ class Solution:
             
         n=len(s)
         m=len(t)
-        dp=[[-1 for j in range(m)] for i in range(n)]
-        ans = f(n-1,m-1,s,t,dp)
+        dp=[[-1 for j in range(m+1)] for i in range(n+1)]
+        ans = f(n,m,s,t,dp)
         return ans
         
