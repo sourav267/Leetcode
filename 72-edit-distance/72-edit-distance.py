a0bbf6b6fda257cfc90dccf398a804df47c3a75f
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-21 06:08:59 +0530

    Time: 135 ms (89.88%), Space: 17.1 MB (77.25%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -2,16 +2,16 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
         def f(i, j, s1, s2, dp):
-            if i < 0:
-                return j+1
+            if i == 0:
+                return j
 
-            if j < 0:
-                return i+1
+            if j == 0:
+                return i
 
             if dp[i][j] != -1:
                 return dp[i][j]
 
-            if s1[i] == s2[j]:
+            if s1[i-1] == s2[j-1]:
                 dp[i][j] = f(i-1, j-1, s1, s2, dp)
                 return dp[i][j]
 
@@ -24,6 +24,6 @@ class Solution:
         
         n = len(word1)
         m = len(word2)
-        dp = [[-1 for j in range(m)] for i in range(n)]
-        ans = f(n-1, m-1, word1, word2, dp)
+        dp = [[-1 for j in range(m+1)] for i in range(n+1)]
+        ans = f(n, m, word1, word2, dp)
         return ans
