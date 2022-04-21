c83456701acdba9cc2a16a0172918d21fea1a968
sourav267 <57962756+sourav267@users.noreply.github.com>
2022-04-21 06:23:05 +0530

    Time: 204 ms (56.48%), Space: 17.6 MB (39.40%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,29 +1,50 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        
-        def f(i, j, s1, s2, dp):
-            if i == 0:
-                return j
-
-            if j == 0:
-                return i
-
-            if dp[i][j] != -1:
-                return dp[i][j]
-
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = f(i-1, j-1, s1, s2, dp)
-                return dp[i][j]
-
-            insert = 1 + f(i, j-1, s1, s2, dp)
-            delete = 1 + f(i-1, j, s1, s2, dp)
-            replace = 1 + f(i-1, j-1, s1, s2, dp)
-
-            dp[i][j] = min(insert, delete, replace)
-            return dp[i][j]
-        
         n = len(word1)
         m = len(word2)
-        dp = [[-1 for j in range(m+1)] for i in range(n+1)]
-        ans = f(n, m, word1, word2, dp)
-        return ans
+        dp = [[0 for j in range(m+1)] for i in range(n+1)]
+        for j in range(m+1):
+            dp[0][j] = j
+
+        for i in range(n+1):
+            dp[i][0] = i
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    insert = dp[i][j - 1]
+                    delete = dp[i-1][j]
+                    replace = dp[i-1][j - 1]
+                    dp[i][j] = 1 + min(insert, delete, replace)
+        
+        return dp[n][m]
+
+#         def f(i, j, s1, s2, dp):
+#             if i == 0:
+#                 return j
+
+#             if j == 0:
+#                 return i
+
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+
+#             if s1[i-1] == s2[j-1]:
+#                 dp[i][j] = f(i-1, j-1, s1, s2, dp)
+#                 return dp[i][j]
+
+#             insert = 1 + f(i, j-1, s1, s2, dp)
+#             delete = 1 + f(i-1, j, s1, s2, dp)
+#             replace = 1 + f(i-1, j-1, s1, s2, dp)
+
+#             dp[i][j] = min(insert, delete, replace)
+#             return dp[i][j]
+        
+        # n = len(word1)
+        # m = len(word2)
+        # dp = [[-1 for j in range(m+1)] for i in range(n+1)]
+        # ans = f(n, m, word1, word2, dp)
+        # return ans
